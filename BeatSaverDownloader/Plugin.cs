e3c330d5863d9ee67828bc48e9e6673c24b31669
andruzzzhka <andruzzzhka@yandex.ru>
2019-01-20 12:53:34 +0700

    Bumped version to 3.2.2

--- BeatSaverDownloader/Plugin.cs
+++ BeatSaverDownloader/Plugin.cs
@@ -15,7 +15,7 @@ namespace BeatSaverDownloader
     {
         string IPlugin.Name { get { return "BeatSaver Downloader"; } }
 
-        string IPlugin.Version { get { return "3.2.1"; } }
+        string IPlugin.Version { get { return "3.2.2"; } }
         
         public void OnApplicationQuit()
         {
